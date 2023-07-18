47cccc310a029062572d57794dc686d7654b1255
Sharefree2222 <95672214+Sharefree2222@users.noreply.github.com>
2023-07-18 11:11:13 +0700

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
+    $("#showNormal").click(function(){
+        $("#normalContent").show();
+        $("#vipContent").hide();
+    });
 
+    $("#showVip").click(function(){
+        $("#normalContent").hide();
+        $("#vipContent").show();
+    });
+
+    $("#timeout-button").click(function(){
+        setTimeout(function(){ 
+            window.location.href = "/"; 
+        }, 3600000); 
+    });
+});
+
+$(document).ready(function() {
+  var modalShown = localStorage.getItem('modalShown');
+  if (!modalShown) {
+    $('#exampleModal').modal('show');
+    localStorage.setItem('modalShown', 'yes');
+  }
+});
